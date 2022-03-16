1e682b27a48f1fcb93561d7d8a29abbfd7baf495
unknown <fajarmh2@gmail.com>
2022-03-16 17:33:43 +0700

    Modified home with for loop ejs

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-  console.log(posts);
-  res.render("home", {contentStart: homeStartingContent});
+  res.render("home", {contentStart: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function(req, res) {
